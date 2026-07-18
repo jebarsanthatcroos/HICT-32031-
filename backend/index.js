5deb823a4a1a6d6f2e1447e51fbadc2615541459
jebrsan thatcroos <jebarsanthatcroos@gmail.com>
2026-07-18 22:40:48 +0530

    updathe codeng

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,19 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
 
 const tasksRoute = require('./routes/tasks');
 app.use('/api/tasks', tasksRoute);
@@ -22,4 +25,4 @@ const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-module.exports = { app, server}
+module.exports = { app, server };
