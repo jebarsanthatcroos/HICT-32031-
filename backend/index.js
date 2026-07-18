9f58fe9e1d3b6373f0ca53807646da899cd56654
jebrsan thatcroos <jebarsanthatcroos@gmail.com>
2026-07-18 21:02:10 +0530

    fix the Error

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 const tasksRoute = require('./routes/tasks');
 app.use('/api/tasks', tasksRoute);
 
 const server = app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
-module.exports = { app, server };
+module.exports = { app, server}
